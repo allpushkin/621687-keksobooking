c9437b9c28d7837c7f446c5befdba5da275bc1fb
Sergeyingit <sergeyakkaynt@gmail.com>
2018-12-20 22:33:48 +0300

    Контрольный коммит

--- js/valid.js
+++ js/valid.js
@@ -33,79 +33,59 @@ timeElements.addEventListener('input', function (evt) {
   var timeElem = evt.target;
   var targetValue = evt.target.value;
   var timeValue;
-  //if (timeElem === timeIn) {
-    switch (targetValue) {
-      case '12:00':
-        timeValue = '12:00';
-        break;
-      case '13:00':
-        timeValue = '13:00';
-        break;
-      case '14:00':
-        timeValue = '14:00';
-        break;
-    }
+
+  switch (targetValue) {
+    case '12:00':
+      timeValue = '12:00';
+      break;
+    case '13:00':
+      timeValue = '13:00';
+      break;
+    case '14:00':
+      timeValue = '14:00';
+      break;
+  }
   if (timeElem === timeIn) {
-  timeOut.value = timeValue;
+    timeOut.value = timeValue;
   }
   timeIn.value = timeValue;
 });
 
-/*timeOut.addEventListener('input', function (evt) {
-  var targetValue = evt.target.value;
-  var timeInValue;
-  switch (targetValue) {
-    case '12:00':
-      timeInValue = '12:00';
-      break;
-    case '13:00':
-      timeInValue = '13:00';
-      break;
-    case '14:00':
-      timeInValue = '14:00';
-      break;
-  }
-  timeIn.value = timeInValue;
-}); */
-
 var adForm = document.querySelector('.ad-form');
 var title = adForm.querySelector('input[name=title]');
 
+//  событие на время и цену
+title.addEventListener('invalid', validTitle);
 
-// вот так работает, если слушать отдельные поля, но с косяками.
-// но ты писал вешать инвалид на всю форму. тут я видимо что-то делаю не так, потому что в таком случае не работает ничего
-title.addEventListener('invalid', function () {
-  validTitle();
-
-});
-
-price.addEventListener('invalid', function () {
-  validPrice();
-});
+price.addEventListener('invalid', validPrice);
 
 var validTitle = function () {
+  var textError = '';
   if (title.validity.tooShort) {
-    title.setCustomValidity('Минимальная длина заголовка - 30 символов');
+    textError = 'Минимальная длина заголовка - 30 символов';
   } else if (title.validity.tooLong) {
-    title.setCustomValidity('Максимальная длина — 100 символов');
+    textError = 'Максимальная длина — 100 символов';
   } else if (title.validity.valueMissing) {
-    title.setCustomValidity('Обязательное поле');
-  } else {
-    title.setCustomValidity('');
+    textError = 'Обязательное поле';
   }
+  title.setCustomValidity(textError);
 };
 
 var validPrice = function (evt) {
-  var target = evt.price.value;
+  var targetMin = evt.price.min;
+  var textError = '';
   if (price.validity.rangeUnderflow) {
-    price.setCustomValidity('Минимальная цена за ночь - ' + target);
+    textError = 'Минимальная цена за ночь - ' + targetMin;
+
   } else if (price.validity.rangeOverflow) {
-    price.setCustomValidity('Максимальная цена за ночь — 1000000');
+    textError = 'Максимальная цена за ночь — 1000000';
+
   } else if (price.validity.valueMissing) {
-    price.setCustomValidity('Обязательное поле');
-  } else if (price.validity.typeMismatch) {
-    price.setCustomValidity('Неверный тип значения');
-  } else {
-    price.setCustomValidity('');
+    textError = 'Обязательное поле';
+
+  } else if (!price.validity.typeMismatch) {
+    textError = 'Неверный тип значения';
   }
+  price.setCustomValidity(textError);
+
 };
