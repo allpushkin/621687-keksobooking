690076aafd2d2826109c359d39c6e30472e2f645
Sergeyingit <sergeyakkaynt@gmail.com>
2018-12-20 23:31:36 +0300

    Последние правки

--- js/valid.js
+++ js/valid.js
@@ -53,39 +53,35 @@ timeElements.addEventListener('input', function (evt) {
 
 var adForm = document.querySelector('.ad-form');
 var title = adForm.querySelector('input[name=title]');
+// валидация заголовка
+var validTitle = function (evt) {
+  var target = evt.target;
+  var textError = '';
+  if (target.validity.tooShort) {
+    textError = 'Минимальная длина заголовка - 30 символов';
+  } else if (target.validity.tooLong) {
+    textError = 'Максимальная длина заголовка — 100 символов';
+  } else if (target.validity.valueMissing) {
+    textError = 'Обязательное поле';
+  }
+  target.setCustomValidity(textError);
+};
+// валидация цены
+var validPrice = function (evt) {
+  var target = evt.target;
+  var targetMin = evt.target.min;
+  var textError = '';
+  if (target.validity.rangeUnderflow) {
+    textError = 'Минимальная цена за ночь - ' + targetMin;
+  } else if (target.validity.rangeOverflow) {
+    textError = 'Максимальная цена за ночь — 1000000';
+  } else if (target.validity.valueMissing) {
+    textError = 'Обязательное поле';
+  }
+  target.setCustomValidity(textError);
+};
 
 //  событие на время и цену
 title.addEventListener('invalid', validTitle);
 
 price.addEventListener('invalid', validPrice);
-
-var validTitle = function () {
-  var textError = '';
-  if (title.validity.tooShort) {
-    textError = 'Минимальная длина заголовка - 30 символов';
-  } else if (title.validity.tooLong) {
-    textError = 'Максимальная длина — 100 символов';
-  } else if (title.validity.valueMissing) {
-    textError = 'Обязательное поле';
-  }
-  title.setCustomValidity(textError);
-};
-
-var validPrice = function (evt) {
-  var targetMin = evt.price.min;
-  var textError = '';
-  if (price.validity.rangeUnderflow) {
-    textError = 'Минимальная цена за ночь - ' + targetMin;
-
-  } else if (price.validity.rangeOverflow) {
-    textError = 'Максимальная цена за ночь — 1000000';
-
-  } else if (price.validity.valueMissing) {
-    textError = 'Обязательное поле';
-
-  } else if (!price.validity.typeMismatch) {
-    textError = 'Неверный тип значения';
-  }
-  price.setCustomValidity(textError);
-
-};
