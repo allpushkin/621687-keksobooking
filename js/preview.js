4f1b1e1d0ee59e73da3872e262cde62cce60ca97
Sergey <sergeyakkaynt@gmail.com>
2019-01-25 14:15:19 +0300

    меняет начальное значения в выборе кол-ва гостей, добавляет модуль загрузки аватарки на страницу

---
+++ js/preview.js
@@ -0,0 +1,48 @@
+'use strict';
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+  var fileChooserAdvert = document.querySelector('.ad-form-header__input');
+  var previewAdvert = document.querySelector('.ad-form-header__preview img');
+
+  fileChooserAdvert.addEventListener('change', function () {
+    var file = fileChooserAdvert.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+     return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        previewAdvert.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+
+  var fileChooserAdvert = document.querySelector('.ad-form__input');
+  var previewAdvert = document.querySelector('.ad-form__photo img');
+
+  fileChooserAdvert.addEventListener('change', function () {
+    var file = fileChooserAdvert.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+     return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        previewAdvert.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+})();
